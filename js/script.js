6918404a578b8ae0e1c2cacfe495e87199726b2f
FernandoSilvera <fernando.silvera02@gmail.com>
2022-05-30 16:57:53 +0200

    Excercise almost completed, miss pints and endgame.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const grid = document.getElementById("grid");
+let points = 0;
+
+function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 function createGrid(xCellsNum, yCellsNum) {
   const cellsNum = xCellsNum * yCellsNum;
   grid.style.width = `calc(var(--cell-size) * ${xCellsNum})`;
+  let bombs = [];
+
+  // Bombs generator
+  for (let i = 1; i <= 16; i++) {
+    bombs.push(getRandomNum(1, cellsNum));
+  }
 
   // Grid creation
   for (let i = 1; i <= cellsNum; i++) {
@@ -17,12 +31,22 @@ function createGrid(xCellsNum, yCellsNum) {
     // On click event
     cell.addEventListener("click", function () {
 
-      // Make the cell lightblue
-      this.classList.add("bg-info")
+      // Check if there's a bomb or not
+      for (let j = 0; j < bombs.length; j++) {
+        if (this.innerHTML === `<span>${bombs[j]}</span>`) {
+          // Make the cell red
+          this.classList.add("bg-danger")
+        } else {
+          // Make the cell lightblue
+          this.classList.add("bg-info")
+        }
+      }
     });
 
     grid.append(cell);
   }
+
+  
 }
 
 createGrid(10, 10);
